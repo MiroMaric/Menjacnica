517938c52cf8c1964ef088cc333d858f8ffad33a
MiroMaric <miromaric1996@gmail.com>
2017-03-23 20:10:39 +0100

    U sve tri klase su redefinasane toString,equals i hashCode medote

--- Menjacnica/src/valute/Valuta.java
+++ Menjacnica/src/valute/Valuta.java
@@ -41,10 +41,43 @@ public class Valuta {
 	public void setKurs(Kurs kurs) {
 		this.kurs = kurs;
 	}
-	
-	
-
-	
-
-	
+	@Override
+	public String toString() {
+		return "Naziv valute: " + naziv + "\n Skracenica: " + skracenNaziv + "\n kurs: " + kurs;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kurs == null) ? 0 : kurs.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skracenNaziv == null) ? 0 : skracenNaziv.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (kurs == null) {
+			if (other.kurs != null)
+				return false;
+		} else if (!kurs.equals(other.kurs))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (skracenNaziv == null) {
+			if (other.skracenNaziv != null)
+				return false;
+		} else if (!skracenNaziv.equals(other.skracenNaziv))
+			return false;
+		return true;
+	}
 }
